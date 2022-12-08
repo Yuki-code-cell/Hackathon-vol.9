af9e26194790e5d68462bf5fe240bc71f25a0fbc
Yuki-code-cell <yuki.kaneko809@gmail.com>
2022-12-08 20:14:58 +0900

    update hooks

--- hooks/useWikipedia.ts
+++ hooks/useWikipedia.ts
@@ -1,24 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import wikipedia from 'wikipedia-js';
+import * as wikipedia from 'wikipedia-js';
 
-function useWikipedia(topic) {
-	const [results, setResults] = useState([]);
+declare module 'wikipedia-js' {
+  export function search(term: string): Promise<any>;
+  export function getArticle(title: string): Promise<any>;
+}
 
-	useEffect(() => {
-    wikipedia.searchArticle({ query: topic, format: 'json' }, (err, jsonWikiData) => {
-      if (err) {
-        console.error(err);
-        return;
+function useWikipedia(term: string) {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const endpoint = 'https://en.wikipedia.org/w/api.php';
+    const params = {
+      action: 'query',
+      list: 'search',
+      srsearch: term,
+      format: 'json',
+    };
+
+    fetch(endpoint, {
+      method: 'GET',
+      params,
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch Wikipedia API');
       }
-      const articles = jsonWikiData.query.search;
-      const updatedResults = articles.map(article => ({
-        title: article.title,
-        snippet: article.snippet,
-        url: https://ja.wikipedia.org/wiki/${encodeURIComponent(article.title)},
-      }));
-      setResults(updatedResults);
+    }).then(data => {
+      const results = data.query.search;
+      setResults(results);
+    }).catch(error => {
+      console.error(error);
     });
-  }, [topic]);
+  }, [term]);
 
   return results;
 }
+
+// function useWikipedia(topic:any) {
+// 	const [results, setResults] = useState([]);
+
+// 	useEffect(() => {
+//     wikipedia.searchArticle({ query: topic, format: 'json' }, (err, jsonWikiData) => {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       const articles = jsonWikiData.query.search;
+//       const updatedResults = articles.map(article => ({
+//         title: article.title,
+//         snippet: article.snippet,
+//         url: https://ja.wikipedia.org/wiki/${encodeURIComponent(article.title)},
+//       }));
+//       setResults(updatedResults);
+//     });
+//   }, [topic]);
+
+//   return results;
+// }
