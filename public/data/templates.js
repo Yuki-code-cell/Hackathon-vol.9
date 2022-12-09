d30dd16633877515e961b8a2f2f7b67025ed0782
Yuki-code-cell <yuki.kaneko809@gmail.com>
2022-12-10 03:47:54 +0900

    update templates

---
+++ public/data/templates.js
@@ -0,0 +1,44 @@
+const DataArray = [
+  { url: "https://111.com", comment: 1, },
+  { url: "https://222.com", comment: 2, },
+  { url: "https://333.com", comment: 3, }
+
+];
+module.exports = DataArray;
+
+console.log(DataArray[0][0]);
+console.log(DataArray[1][0]);
+console.log(DataArray[2][0]);
+
+// [
+//   {
+//     "theme": "template1",
+//     "description": "templateの一番目です",
+//     "faq": [
+//       {
+//         "question": "一番目の質問です",
+//         "answer": ""
+//       }
+//     ]
+//   },
+//   {
+//     "theme": "template2",
+//     "description": "templateの一番目です",
+//     "faq": [
+//       {
+//         "question": "一番目の質問です",
+//         "answer": ""
+//       }
+//     ]
+//   },
+//   {
+//     "theme": "template3",
+//     "description": "templateの一番目です",
+//     "faq": [
+//       {
+//         "question": "一番目の質問です",
+//         "answer": ""
+//       }
+//     ]
+//   }
+// ]
