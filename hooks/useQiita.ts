c48fe7b82c7d38975d62c647b641f8c2e8bb0240
nisi0929 <kengo279@icloud.com>
2022-12-11 14:24:45 +0900

    error

--- hooks/useQiita.ts
+++ hooks/useQiita.ts
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const useQiita = (?query=${検索ワード}) => {
+export const useQiita = () => {
   const [articles, setArticles] = useState([]);
 
   const fetchArticles = async () => {
     try {
-      const response = await axios.get('https://qiita.com/api/v2/items?query=${引数の名前}');
+      const response = await axios.get(
+        'https://qiita.com/api/v2/items?query=${引数の名前}'
+      );
 
       setArticles(response.data);
     } catch (error) {
@@ -18,13 +20,13 @@ export const useQiita = (?query=${検索ワード}) => {
     console.log(fetchArticles());
   }, []);
 
-  return (
-    <ul>
-      {articles.map((article) => (
-        <li key={article.id}>{article.title}</li>
-      ))}
-    </ul>
-  );
+  // return (
+  //   <ul>
+  //     {articles.map((article) => (
+  //       <li key={article.id}>{article.title}</li>
+  //     ))}
+  //   </ul>
+  // );
 };
 
 export default useQiita;
